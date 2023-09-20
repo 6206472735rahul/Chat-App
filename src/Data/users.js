cfe7a3322bbe47bc4fbb83e268451f08f30addcb
Rahul <6206472735rahul@gmail.com>
2023-09-20 15:13:45 +0530

    first commit

---
+++ src/Data/users.js
@@ -0,0 +1,301 @@
+import user from '../reducers/user';
+ 
+export const data = {
+    profile: {
+      id: 1,
+      name: "Rahul Kumar",
+      image:user.image,
+      contacts: [
+        {
+          id: 2,
+          name: "Ajit kumar",
+          image:
+           "https://e1.pxfuel.com/desktop-wallpaper/604/935/desktop-wallpaper-boy-and-girl-love-cartoon-love-in-cartoon-thumbnail.jpg",
+          chatlog: [
+            {
+              text: "Hello brother,How are you ?",
+              timestamp: "12:36 PM",
+              sender: "user",
+              message_id: 1,
+            },
+            {
+              text: "I am fine Ajit. What about you ?",
+              timestamp: "12:37 PM",
+              sender: "me",
+              message_id: 2,
+            },
+            {
+              text: "I am great, Ajit.",
+              timestamp: "1:00 PM",
+              sender: "user",
+              message_id: 3,
+            },
+            {
+              text: "lets meet",
+              timestamp: "01:01 PM",
+              sender: "user",
+              message_id: 4,
+            },
+            {
+              text: "Okay bro",
+              timestamp: "01:05 PM",
+              sender: "me",
+              message_id: 5,
+            },
+          ],
+        },
+        {
+          id: 3,
+          name: "Shubham Singh",
+          image:"https://w0.peakpx.com/wallpaper/317/554/HD-wallpaper-ntr-man-of-masses-jr-ntr-manofmasses-mass-ramcharan-swag-jrntr-thumbnail.jpg",
+          chatlog: [
+            {
+              text: "Hii bro",
+              timestamp: "3:00 PM",
+              sender: "user",
+              message_id: 1,
+            },
+            {
+              text: "How are you,",
+              timestamp: "3:02 PM",
+              sender: "me",
+              message_id: 2,
+            },
+            {
+              text: " i am fine But Bro Actually i Need a some money.",
+              timestamp: "3:03 PM",
+              sender: "user",
+              message_id: 3,
+            },
+            {
+              text: "if you have then send me bro Because its very important Because i am on the way but i do not have money",
+              timestamp: "3:30 PM",
+              sender: "user",
+              message_id: 4,
+            },
+            {
+              text: "Ok i will try bro, Don't worry ?",
+              timestamp: "4:00 PM",
+              sender: "me",
+              message_id: 5,
+            },
+            {
+              text: "Bro i tried,But Noboddy helped me ,What will i do.. ?",
+              timestamp: "4:00 PM",
+              sender: "me",
+              message_id: 5,
+            },
+            {
+              text: "Ok no problem bro...",
+              timestamp: "3:03 PM",
+              sender: "user",
+              message_id: 3,
+            },
+          ],
+        },
+        {
+          id: 4,
+          name: "Krishnkant",
+          image:
+            "https://st1.bollywoodlife.com/wp-content/uploads/2020/12/Katrina-Kaif6.jpg",
+          chatlog: [
+            {
+              text: "Hi Rahul,!",
+              timestamp: "4:30 PM",
+              sender: "user",
+              message_id: 1,
+            },
+            {
+              text: "yes speak bro.How are you ?.",
+              timestamp: "04:32 PM",
+              sender: "me",
+              message_id: 2,
+            },
+            {
+              text: "I am great ,  Rahul.",
+              timestamp: "04:40 PM",
+              sender: "user",
+              message_id: 3,
+            },
+            {
+              text: "Are you busy?",
+              timestamp: "04:45 PM",
+              sender: "user",
+              message_id: 4,
+            },
+            {
+              text: "  Yes i am busy because i am making chat App ",
+              timestamp: "04:50 PM",
+              sender: "me",
+              message_id: 5,
+            },
+          ],
+        },
+        {
+          id: 5,
+          name: "Shiva Kumar",
+          image:
+          "https://www.photosbin.com/uploads/ugau31012020113926.jpg",
+          chatlog: [
+            {
+              text: "Hi Rahul!",
+              timestamp: "4:30 PM",
+              sender: "user",
+              message_id: 1,
+            },
+            {
+              text: "yes speak bro.How are you ?.",
+              timestamp: "04:32 PM",
+              sender: "me",
+              message_id: 2,
+            },
+            {
+              text: "I am great,Rahul.",
+              timestamp: "04:40 PM",
+              sender: "user",
+              message_id: 3,
+            },
+            {
+              text: "Are you busy?",
+              timestamp: "04:45 PM",
+              sender: "user",
+              message_id: 4,
+            },
+            {
+              text: "  No i am not busy",
+              timestamp: "04:50 PM",
+              sender: "me",
+              message_id: 5,
+            },
+            {
+              text: "Tell me any problem",
+              timestamp: "04:50 PM",
+              sender: "me",
+              message_id: 5,
+            },
+            {
+              text: "Yes bro. My bag stolen in merrut what would i do give me any idea",
+              timestamp: "04:50 PM",
+              sender: "user",
+              message_id: 5,
+            },
+            {
+              text: "Go to police station and do complain bro..",
+              timestamp: "04:50 PM",
+              sender: "me",
+              message_id: 5,
+            },
+            {
+              text: "Ok bro",
+              timestamp: "04:50 PM",
+              sender: "user",
+              message_id: 5,
+            },
+        
+          ],
+    
+        },
+        {
+          id: 6,
+          name: "Arun kumar",
+          image:
+            "https://i.pinimg.com/1200x/9d/79/6a/9d796a91074badf6fa055d3d26928dd3.jpg",
+          chatlog: [
+            {
+              text: "Hey Rahul, where is your friend ?.",
+              timestamp: "05:00 PM",
+              sender: "user",
+              message_id: 1,
+            },
+            {
+              text: "I don't know bro!",
+              timestamp: "05:01 PM", 
+              sender: "me",
+              message_id: 2,
+            },
+            {
+              text: "Are you sure?.",
+              timestamp: "05:05 PM",
+              sender: "user",
+              message_id: 3,
+            },
+            {
+              text: "Yes bro, I am sure",
+              timestamp: "05:07 PM",
+              sender: "me",
+              message_id: 4,
+            },
+            {
+              text: "Ok bro byy",
+              timestamp: "05:09 PM",
+              sender: "me",
+              message_id: 5,
+            },
+            {
+              text: "Ok byy and Tc..",
+              timestamp: "05:07 PM",
+              sender: "uesr",
+              message_id: 4,
+            },
+          ],
+        },
+        {
+          id: 7,
+          name: "Parsant kumar",
+          image:
+            "https://photosfile.com/wp-content/uploads/2022/02/Yo-Yo-Honey-Singh-1.jpg",
+          chatlog: [
+            {
+              text: "Where are you bro ?",
+              timestamp: "7:00 PM",
+              sender: "me",
+              message_id: 1,
+            },
+            {
+              text: "I'm in Market now",
+              timestamp: "07:07 PM",
+              sender: "user",
+              message_id: 2,
+            },
+            {
+              text: "call me ASAP",
+              timestamp: "07:07 PM",
+              sender: "me",
+              message_id: 3,
+            },
+            {
+              text: "ok",
+              timestamp: "07:09 PM",
+              sender: "user",
+              message_id: 4,
+            },
+            {
+              text: "Is everything fine ?",
+              timestamp: "07:10 PM",
+              sender: "me",
+              message_id: 5,
+            },
+            {
+              text: "Yeah !!",
+              timestamp: "07:12 PM",
+              sender: "user",
+              message_id: 6,
+            },
+          ],
+        },
+        {
+          id: 8,
+          name: "Vikram Singh",
+          image:"https://upload.wikimedia.org/wikipedia/commons/7/77/Lotha_Naga_girl.jpg",
+          chatlog: [],
+        },
+        {
+          id: 9,
+          name: "Abhishek kumar",
+          image:
+            "https://wallpapers.com/images/hd/frames-ms-dhoni-hd-sw7j6i0wk1yqn53c.jpg",
+          chatlog: [],
+        },
+      ],
+    },
+  };
